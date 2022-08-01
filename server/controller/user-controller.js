817cb491c799f9851910a799d2877a23e4c19ae9
Nitish8800 <nitishfea12-784@gmail.com>
2022-08-01 13:37:39 +0530

    SIGNUP API CREATE

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -11,12 +11,12 @@ export const singupUser = async (request, response) => {
     try {
         // const salt = await bcrypt.genSalt();
         // const hashedPassword = await bcrypt.hash(request.body.password, salt);
-        const hashedPassword = await bcrypt.hash(request.body.password, 10);
+        const hashedPassword = await bcrypt.hash(request.body.password, 12);
 
         const user = { username: request.body.username, name: request.body.name, password: hashedPassword }
 
         const newUser = new User(user);
-        await newUser.save();
+        await newUser.save(); 
 
         return response.status(200).json({ msg: 'Signup successfull' });
     } catch (error) {
