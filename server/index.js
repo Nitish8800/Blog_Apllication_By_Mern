195e7f1a5a9be12cfccd1213db6fe21201557078
Nitish8800 <nitishfea12-784@gmail.com>
2022-08-01 13:09:24 +0530

    create routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import bodyParser from 'body-parser';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 //components
-// import Connection from './database/db.js';
+import Connection from "./database/db.js";
 // import Router from './routes/route.js';
-
 
 dotenv.config();
 
@@ -17,11 +16,9 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use('/', Router);
 
-
 const PORT = 8000;
-// const username = process.env.DB_USERNAME;
-// const password = process.env.DB_PASSWORD;
 
-// Connection(username, password);
-
-app.listen(PORT, () => console.log(`Server is running successfully on the PORT ${PORT}`));
+Connection();
+app.listen(PORT, () =>
+  console.log(`Server is running successfully on the PORT ${PORT}`)
+);
