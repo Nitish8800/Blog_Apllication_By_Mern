148bc6002122b4dcb2d09d640a464bd7d79d8fb6
Nitish8800 <nitishfea12-784@gmail.com>
2022-08-01 09:48:03 +0530

    login page css

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -2,30 +2,112 @@ import React from "react";
 
 import { Box, TextField, Button, Typography, styled } from "@mui/material";
 
+{
+  /* <>-------------    Add CSS in Element               ---------------</> */
+}
+
+const Component = styled(Box)`
+  width: 400px;
+  margin: auto;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+    rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+`;
+
+const Image = styled("img")({
+  width: 100,
+  display: "flex",
+  margin: "auto",
+  padding: "50px 0 0",
+});
+
+const Wrapper = styled(Box)`
+  padding: 25px 35px;
+  display: flex;
+  flex: 1;
+  overflow: auto;
+  flex-direction: column;
+  & > div,
+  & > button,
+  & > p {
+    margin-top: 20px;
+  }
+`;
+
+const LoginButton = styled(Button)`
+  text-transform: none;
+  background: #fb641b;
+  color: #fff;
+  height: 48px;
+  border-radius: 2px;
+`;
+
+const SignupButton = styled(Button)`
+  text-transform: none;
+  background: #fff;
+  color: #2874f0;
+  height: 48px;
+  border-radius: 2px;
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+`;
+
+const Text = styled(Typography)`
+  color: #878787;
+  font-size: 12px;
+`;
+
+const Error = styled(Typography)`
+  font-size: 10px;
+  color: #ff6161;
+  line-height: 0;
+  margin-top: 10px;
+  font-weight: 600;
+`;
+
+{
+  /* <>-------------    Add CSS in Element               ---------------</> */
+}
+
+const loginInitialValues = {
+  username: "",
+  password: "",
+};
+
+const signupInitialValues = {
+  name: "",
+  username: "",
+  password: "",
+};
+
 const Login = () => {
   const imageURL =
     "https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png";
 
   return (
     <div>
-      <Box>
-        <img src={imageURL} alt="blog image" />
+      <Component>
+        <Image src={imageURL} alt="blog image" />
 
         <TextField
-          variant="standard"
+          color="secondary"
+          variant="outlined"
           //   value={login.username}
           //   onChange={(e) => onValueChange(e)}
           name="username"
           label="Enter Username"
         />
         <TextField
-          variant="standard"
+          color="secondary"
+          variant="outlined"
           //   value={login.password}
           //   onChange={(e) => onValueChange(e)}
           name="password"
           label="Enter Password"
         />
-      </Box>
+
+        <Button variant="contained">Login</Button>
+        <Button>Create an account</Button>
+      </Component>
     </div>
   );
 };
