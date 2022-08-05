5741f273bf8f6f9e05d831e212b1581d7ca7943a
Nitish8800 <nitishfea12-784@gmail.com>
2022-08-05 06:52:01 +0530

    done

--- server/client/README.md
+++
@@ -1,1 +0,0 @@
-# Blog_Apllication_By_Mern
--- server/client/src/constants/data.js
+++
@@ -1,7 +0,0 @@
-export const categories = [
-  { id: 1, type: "Fashion" },
-  { id: 2, type: "Movies" },
-  { id: 3, type: "Sports" },
-  { id: 4, type: "Music" },
-  { id: 5, type: "Tech" },
-];
