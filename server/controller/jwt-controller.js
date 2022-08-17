6ab0e41623dab064467f02d7283bf39d077ed603
Nitish Kumar <81190422+Nitish8800@users.noreply.github.com>
2022-08-17 01:52:03 -0700

    Update jwt-controller.js

--- server/controller/jwt-controller.js
+++ server/controller/jwt-controller.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import Token from "../model/token.js";
 
 dotenv.config();
-
 export const authenticateToken = (request, response, next) => {
   const authHeader = request.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
@@ -12,12 +11,10 @@ export const authenticateToken = (request, response, next) => {
   if (token == null) {
     return response.status(401).json({ msg: "token is missing" });
   }
-
   jwt.verify(token, process.env.ACCESS_SECRET_KEY, (error, user) => {
     if (error) {
       return response.status(403).json({ msg: "invalid token" });
     }
-
     request.user = user;
     next();
   });
@@ -25,17 +22,13 @@ export const authenticateToken = (request, response, next) => {
 
 export const createNewToken = async (request, response) => {
   const refreshToken = request.body.token.split(" ")[1];
-
   if (!refreshToken) {
     return response.status(401).json({ msg: "Refresh token is missing" });
   }
-
   const token = await Token.findOne({ token: refreshToken });
-
   if (!token) {
     return response.status(404).json({ msg: "Refresh token is not valid" });
   }
-
   jwt.verify(token.token, process.env.REFRESH_SECRET_KEY, (error, user) => {
     if (error) {
       response.status(500).json({ msg: "invalid refresh token" });
